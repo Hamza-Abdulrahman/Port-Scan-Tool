86942d93f2d36fcf32d2dc84c8fbb08363c70b5f
Hamza-Abdulrahman <75262372+Hamza-Abdulrahman@users.noreply.github.com>
2021-01-18 22:31:22 +0000

    This is a simple tool that enables you to check the activated services

--- ScanManPort.py
+++ ScanManPort.py
@@ -2,6 +2,7 @@
 
 import socket
 from optparse import *
+import time
 
 parser = OptionParser("""
 \033[1;33m This is a simple tool that enables you to check the activated services
@@ -20,19 +21,24 @@ parser.add_option("-t","--outtime",dest="out_time",type="int",help="Enter the Ou
 if options.target == None or options.ports == None or options.out_time == None:
     print(parser.usage)
 else:
+    print("Starting ScanManPort 2021 ......")
+    time.sleep(2)
     target = str(options.target)
     ports = int(options.ports)
-    
+    print(f"Host: {target}")
     for p in range(1,ports):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            out_time = int(options.out_time)
-            s.settimeout(out_time)
-            requist = s.connect_ex((target,p))
         
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        out_time = int(options.out_time)
+        s.settimeout(out_time)
+        requist = s.connect_ex((target,p))
+        try:
             if requist == 0 :
                 service = socket.getservbyport(p)
-                print(f"\033[0;35m {p}\033[0;33m is open ------>\033[0;35m {service}")
+                print(f"""
+            PORT    STATE   SERVICE
+            {p}     open    {service}
+                """)
         except:
             continue
-#end 
+#end .^_^.
