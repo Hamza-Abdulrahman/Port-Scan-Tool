0fc30c752c80c6d5957769f8896e7cab8327fd0f
Abdallah Elsharif <elsharifabdallah53@gmail.com>
2021-01-24 00:03:48 -0500

    Add save output feature

--- ScanManPort.py
+++ ScanManPort.py
@@ -18,11 +18,12 @@ Use -h to ask for help
 parser.add_option("-i","--target",dest="target",type="string",help="Enter your target")
 parser.add_option("-p","--ports",dest="ports",default="1-1024",help="Enter the Range of Ports that you want to check it Ex(1-1024)")
 parser.add_option("-t","--timeout",dest="timeout",default=0.5,help="Enter the Time Out that you want")
-parser.add_option("-v","--software-version",action="store_true",dest="software_version",default=False,help="Get Softwares version")
+parser.add_option("-v","--services-version",action="store_true",dest="services_version",default=False,help="Get Services version")
+parser.add_option("-o","--output",dest="output",help="Enter a FileName for save output")
 
 (options,args) = parser.parse_args()
 
-if not options.target or (not options.ports or not options.timeout and not options.target):
+if not options.target :
     print(parser.usage)
 else:
     print("Starting ScanManPort 2021 ......")
@@ -32,6 +33,9 @@ else:
         start_range , end_range = str(options.ports).split("-")
     else:
         start_range = end_range = options.ports
+
+    if options.output:
+        file = open(options.output,'a')
     
     print(f"Host: {target}\n")
     
@@ -46,9 +50,18 @@ else:
                 service = enumeration.get_service_name(p)
                 print(f"\033[0;34mPORT  \033[0;37m{p}  \033[0;34mSTATE  \033[0;37mopen  \033[0;34m",end='')
                 print(f"SERVICE  \033[0;37m{service}",end = '')
-                if options.software_version:
+                if options.output:
+                    file.write(f"{service}:{target}:{p}")
+
+                if options.services_version:
                     version = enumeration.get_service_version(s,target,p)
                     print(f"  \033[0;34mVERSION  \033[0;37m{version}",end='')
+                    if options.output: 
+                        file.write(f":{version}")
+
+                if options.output:
+                    file.write("\n")
+
                 print()
         
         except KeyboardInterrupt:
@@ -60,6 +73,8 @@ else:
 
         finally:
             s.close()
+
+    if options.output: file.close()
 
 print("\nScan completed successfully")
 
